d46e3e4ef23836be56551aa980de6d330424e6c9
helmi wirata <helmiwirata136@gmail.com>
2026-08-06 02:27:47 +0700

    debug

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
         playsInline
         className="absolute inset-0 h-full w-full object-cover"
       >
-        <source src="/videos/bg3.MOV" type="video/mp4" />
+        <source src="/videos/background.mp4" type="video/mp4" />
       </video>
 
       {/* Overlay */}
